eba4d348089f142cf527455bcf1b8c574a503303
Davi-Dev25 <oliveirasousadavi58@gmail.com>
2025-02-22 21:20:40 -0300

    exercicio 2

---
+++ curso-js/exercicios/aula2/index.js
@@ -0,0 +1,25 @@
+/*Var
+mesma coisa do let, só que mais antigo e menos utilizado e recomendado
+*/
+
+
+
+/*const
+Um tipo de declarar uma variavel onde ela se mantém constante e imutavel, onde não se pode trocar o valor atribuido a ela*/
+const numero = "99"
+
+
+
+/*let
+Um tipo de declarar uma variavel onde se pode trocar o valor dela durante o programa
+*/
+let texto = "olá, mundo"
+
+texto ="se lascar"
+
+alert(texto)
+
+alert(numero)
+
+
+
