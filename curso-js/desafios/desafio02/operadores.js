3649f297f274aae3798944373c8332ffbdbc6411
Davi-Dev25 <oliveirasousadavi58@gmail.com>
2025-02-26 20:55:43 -0300

    desafios de operações e frase

---
+++ curso-js/desafios/desafio02/operadores.js
@@ -0,0 +1,13 @@
+let n1 = Number (prompt ("Digite um número"))
+let n2 = Number (prompt ("Digite outro número"))
+
+
+
+alert 
+(`
+A soma dos números que você colocou é ${n1 + n2},
+A Subtração dos números que você colocou é ${n1 - n2},
+A multiplicação dos números que você colocou é ${n1 * n2},
+A divisão dos números que você colocou é ${n1 / n2},
+`,
+)
