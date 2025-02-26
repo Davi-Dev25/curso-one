f01fcd31ef372bfc85291e7ca556ab43fe4469e5
Davi-Dev25 <oliveirasousadavi58@gmail.com>
2025-02-26 20:11:19 -0300

    exercicio02

--- curso-js/exercicios/aula2/index.js
+++ curso-js/exercicios/aula2/index.js
@@ -5,13 +5,13 @@ mesma coisa do let, só que mais antigo e menos utilizado e recomendado
 
 
 /*const
-Um tipo de declarar uma variavel onde ela se mantém constante e imutavel, onde não se pode trocar o valor atribuido a ela*/
+Uma forma de declarar uma variavel onde ela se mantém constante e imutavel, onde não se pode trocar o valor atribuido a ela*/
 const numero = "99"
 
 
 
 /*let
-Um tipo de declarar uma variavel onde se pode trocar o valor dela durante o programa
+Uma forma de declarar uma variavel onde se pode trocar o valor dela durante o programa
 */
 let texto = "olá, mundo"
 
---
+++ curso-js/exercicios/aula2/operadores.js
@@ -0,0 +1,8 @@
+
+const nome = prompt("Qual é o seu nome?")
+let idade = prompt("Qual é a sua idade?")
+
+idade = parseFloat(idade)
+
+alert(`Seu nome é ${nome} 
+e ano que vem você terá ${idade + 1} anos`)
