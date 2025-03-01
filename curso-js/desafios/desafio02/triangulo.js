bb3589bc6eebf413cd13a2931c5d37ca3da292dc
Davi-Dev25 <oliveirasousadavi58@gmail.com>
2025-02-28 22:51:48 -0300

    desafio do triangulo

--- curso-js/desafios/desafio02/triangulo.js
+++ curso-js/desafios/desafio02/triangulo.js
@@ -1,4 +1,10 @@
 let b = Number (prompt("Fale o tamanho da base"))
 let a = Number (prompt("Fale a altura da base"))
+let l1 = Number (prompt("Digite o valor do lado do triângulo"))
+let l2 = Number (prompt("Digite o valor do lado do triângulo"))
+let l3 = Number (prompt("Digite o valor do lado do triângulo"))
+let p = Number
 
-alert(`A área do triângulo é ${a * b /2}cm`)
+alert(`A área do triângulo é ${a * b /2}cm,
+E área do perimetro é ${p= l1 + l2 + l3}cm.
+` )
